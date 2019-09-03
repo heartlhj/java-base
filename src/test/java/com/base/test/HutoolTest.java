8fd325b0d7cf2ac25796539dd9f766572599ce1d
heartlhj <754491577@qq.com>
2019-09-03 19:56:06 +0800

    git测试

---
+++ src/test/java/com/base/test/HutoolTest.java
@@ -0,0 +1,25 @@
+package com.base.test;
+
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.system.JavaInfo;
+import org.junit.Test;
+
+/**
+ * @ProjectName: java-base
+ * @Package: com.base.test
+ * @ClassName: HutoolTest
+ * @Author: LiAy
+ * @Description: $END$
+ * @Date: 2019-9-2 15:30
+ * @Version: 1.0
+ */
+public class HutoolTest {
+
+    @Test
+    public void test(){
+        String md5 = "lhj";
+        String s = SecureUtil.md5(md5);
+        System.out.println(s);
+        System.out.println( new JavaInfo().getVersion());
+    }
+}
